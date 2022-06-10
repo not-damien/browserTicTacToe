e8f2fe02d2e41e7b467022c239fa8d539a31d1f9
Damien Cruz <dcruz99@uw.edu>
2022-06-10 01:56:40 -0700

    almost full functionality

--- Scripts/tictac.js
+++ Scripts/tictac.js
@@ -2,6 +2,10 @@ const X_CLASS = 'x'
 const CIRCLE_CLASS ='circle'
 const cellemlements = document.querySelectorAll('[data-cell]')
 const board = document.getElementById('board')
+const winningMessageEL = document.getElementById('winningMessage')
+const winningMessageText = document.querySelector('[data-winning-message-text]')
+const restartbutton = document.getElementById('winning-button')
+restartbutton.onclick = startGame
 const WIN_STATES = [
     [0,1,2],
     [4,3,5],
@@ -17,9 +21,12 @@ let circleTurn
 startGame()
 
 function startGame() {
+    winningMessageEL.classList.remove('show')
     circleTurn = false
     cellemlements.forEach(cell =>{
         cell.addEventListener('click', handleClick, {once: true})
+        cell.classList.remove(CIRCLE_CLASS)
+        cell.classList.remove(X_CLASS)
     })
     setBoardHoverClass()
 
@@ -33,7 +40,14 @@ function handleClick(e){
 
     //check for win
     //check for draw
-    
+    if(checkWin(currentClass)){
+        console.log('some one won')
+        gameOver(false)
+    }
+    if(checkDraw()){
+        console.log('draw')
+        gameOver(true)
+    }
     swapTurns()
     setBoardHoverClass()
     //swtich turns 
@@ -55,4 +69,27 @@ function setBoardHoverClass(){
         board.classList.add(X_CLASS)
     }
 
+}
+
+function checkWin(currentClass){
+    return WIN_STATES.some(combination => {
+        return combination.every(index => {
+            return cellemlements[index].classList.contains(currentClass)
+        })
+    })
+}
+
+function gameOver(tie){
+    if(tie){
+       winningMessageText.innerText = 'Draw!'
+    }else{
+        winningMessageText.innerText =  '${circleTurn ? "o" : "x"}'//todo fix this selector 
+    }
+    winningMessageEL.classList.add('show')
+}
+function checkDraw(){
+    return [...cellemlements].every(cell=>{ 
+        return cell.classList.contains(X_CLASS) ||
+        cell.classList.contains(CIRCLE_CLASS)
+    })
 }

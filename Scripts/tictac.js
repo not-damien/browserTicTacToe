26bb03b2a2ba542eb2d9ed8c106872b07de12478
Damien Cruz <dcruz99@uw.edu>
2022-06-10 20:42:18 -0700

    added comments

--- Scripts/tictac.js
+++ Scripts/tictac.js
@@ -16,10 +16,15 @@ const WIN_STATES = [
     [0,4,8],
     [2,4,6]
 ]
-let circleTurn
+let circleTurn //Boolon false means it is X's turn, true means it is circles turn 
 
+//call to start game to start the game 
 startGame()
 
+
+/**
+ * Gets the game ready to play and reset the board
+ */
 function startGame() {
     winningMessageEL.classList.remove('show')
     circleTurn = false
@@ -31,7 +36,10 @@ function startGame() {
     setBoardHoverClass()
 
 }
-
+/**
+ * game logic for when a playeer clicks selects a position on the board 
+ * @param {*} e 
+ */
 function handleClick(e){
     const cell = e.target
     const currentClass = circleTurn ? CIRCLE_CLASS : X_CLASS
@@ -42,22 +50,30 @@ function handleClick(e){
     //check for draw
     if(checkWin(currentClass)){
         gameOver(false)
-    }
-    if(checkDraw()){
+    }else if(checkDraw()){
         gameOver(true)
     }
     swapTurns()
     setBoardHoverClass()
     //swtich turns 
 }
-
+/**
+ * places the class given into the cell given 
+ * @param {} cell 
+ * @param {*} currentClass 
+ */
 function placeMark(cell, currentClass){
     cell.classList.add(currentClass)
 }
-
+/**
+ * swaps turns 
+ */
 function swapTurns(){
     circleTurn = !circleTurn
 }
+/**
+ * based on whose turn it is this function handles the hoover effect 
+ */
 function setBoardHoverClass(){
     board.classList.remove(X_CLASS)
     board.classList.remove(CIRCLE_CLASS)
@@ -68,7 +84,11 @@ function setBoardHoverClass(){
     }
 
 }
-
+/**
+ * checks if there is a win state based on the current move 
+ * @param {*} currentClass 
+ * @returns 
+ */
 function checkWin(currentClass){
     return WIN_STATES.some(combination => {
         return combination.every(index => {
@@ -76,7 +96,10 @@ function checkWin(currentClass){
         })
     })
 }
-
+/**
+ * ends the game and handles displaying the end screen
+ * @param {*} tie 
+ */
 function gameOver(tie){
     if(tie){
        winningMessageText.innerText = 'Draw!'
@@ -85,6 +108,10 @@ function gameOver(tie){
     }
     winningMessageEL.classList.add('show')
 }
+/**
+ * checks if the game ended in a tie
+ * @returns 
+ */
 function checkDraw(){
     return [...cellemlements].every(cell=>{ 
         return cell.classList.contains(X_CLASS) ||
